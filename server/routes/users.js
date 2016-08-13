29c3dc7bc9c80cea6a7682fb47e35ef7dca8d13e
Rich Churcher <rich.churcher@gmail.com>
2016-08-13 13:45:35 +1200

    GET /users/:id

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,7 +11,22 @@ router.get('/', (req, res) => {
       res.json(users)
     })
     .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
+      res.status(500).send(`DATABASE ERROR: ${err.message}`)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  knex('users')
+    .select()
+    .where('id', req.params.id)
+    .then(user => {
+      if (user.length === 0) {
+        res.sendStatus(404)
+      }
+      res.json(user[0])
+    })
+    .catch(err => {
+      res.status(500).send(`DATABASE ERROR: ${err.message}`)
     })
 })
 
--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -17,6 +17,25 @@ test('/users returns an array', t => {
     })
 })
 
+// Prerequisite: requires seed to have run!
+test('/user/:id returns the correct ', t => {
+  const expected = {
+    id: 99901, 
+    name: 'Ambitious Aardvark', 
+    email: 'aardvark@example.org'
+  }
+  request(app)
+    .get(`/api/users/${expected.id}`)
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      const actual = res.body
+      t.equal(err, null)
+      t.deepEqual(actual, expected)
+      t.end()
+    })
+})
+
 test.onFinish(() => {
   process.exit(0)
 })
